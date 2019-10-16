616051c73351ccfff7e6fd378202c25f9300b37a
alexandergustavsen <alexander_gustavsen@hotmail.com>
2019-10-16 16:14:05 +0200

    Initial Commit

---
+++ Exam-PG5600-Tabbed/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Exam-PG5600-Tabbed
+//
+//  Created by Alexander Gustavsen on 16/10/2019.
+//  Copyright © 2019 Alexander Gustavsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exam-PG5600-Tabbed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Exam-PG5600-Tabbed
+//
+//  Created by Alexander Gustavsen on 16/10/2019.
+//  Copyright © 2019 Alexander Gustavsen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
